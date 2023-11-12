422df79c3c03f7f8e0e6f3b5dfebbbb30da6261c
Zehra Aykan <139782347+aykan001@users.noreply.github.com>
2023-11-12 20:13:41 +0300

    Update README.py

--- README.py
+++ README.py
@@ -1,1 +1,36 @@
-# G-r-nt-_-leme
+import requests
+from PIL import Image
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+image_url = "https://example.com/path/to/your/image.jpg"
+
+
+response = requests.get(image_url)
+img = Image.open(io.BytesIO(response.content)).convert('L')  
+
+
+I = np.array(img)
+
+
+Hist = np.zeros(256)
+
+
+h, w = I.shape
+
+
+for v in range(h):
+    for u in range(w):
+        
+        intensity = I[v, u]
+
+       
+        Hist[intensity] += 1
+
+
+plt.bar(range(256), Hist, color='gray', alpha=0.7)
+plt.title('Histogram')
+plt.xlabel('Pixel Intensity')
+plt.ylabel('Frequency')
+plt.show()
